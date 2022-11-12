6718ea3ac61fe2b9e3575117d010013995bf7488
Gabriele Pappalardo <leleattolo99@gmail.com>
2022-11-12 19:29:04 +0100

    add workers argument for server

--- assignment_2/server/src/main.cpp
+++ assignment_2/server/src/main.cpp
@@ -1,36 +1,51 @@
 #include <stdexcept>
 #include <string>
+
 #include "Server.hpp"
-#include "Common.hpp"
+
+struct Args {
+    size_t hash_table_size = 0;
+    unsigned workers = std::thread::hardware_concurrency();
+};
 
 void print_usage() {
-	std::fprintf(stderr, "usage: ./server <hash-table-size>\n");
+	std::fprintf(stderr, "usage: ./server <hash-table-size> <workers>[default %u]\n", std::thread::hardware_concurrency());
 }
 
-std::size_t parse_argument(char *const *argv) {
-    std::size_t hash_table_size = 0;
+Args parse_arguments(int argc, char *const *argv) {
+
+    Args args;
+
     try {
-        hash_table_size = std::stoul(argv[1], nullptr, 10);
+        args.hash_table_size = std::stoul(argv[1], nullptr, 10);
     }
     catch (const std::invalid_argument &e) {
         fprintf(stderr, "[error] :: Cannot parse HashTable's size correctly.\n");
+        std::exit(EXIT_FAILURE);
     }
-    return hash_table_size;
+
+    if (argc == 3) {
+        try {
+            args.workers = std::stoul(argv[2], nullptr, 10);
+        }
+        catch (const std::invalid_argument &e) {
+            fprintf(stderr, "[error] :: Cannot parse workers number correctly, using default.\n");
+        }
+    }
+
+    return args;
 }
 
 int main(int argc, char **argv) {
 
-	if (argc != 2) {
+	if (argc < 2 || argc > 3) {
 		print_usage();
 		return EXIT_FAILURE;
 	}
 
-    std::size_t hash_table_size = parse_argument(argv);
-    if (hash_table_size == 0) {
-        return EXIT_FAILURE;
-    }
+    auto args = parse_arguments(argc, argv);
 
-    Server<MyString, MyString> server(4, hash_table_size);
+    Server<MyString, MyString> server(args.workers, args.hash_table_size);
     server.start();
 
     return EXIT_SUCCESS;
